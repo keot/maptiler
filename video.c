201a40716aac7ed2368b801261f8ca90fd595447
James Mardell <james.mardell@imperial.ac.uk>
2011-12-05 11:30:11 +0000

    Broken revision with initial new FFMPEG code.

---
+++ video.c
@@ -0,0 +1,9 @@
+//
+//  video.c
+//  maptiler2
+//
+//  Created by James Mardell on 04/12/2011.
+//  Copyright 2011 Student. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ video.h
@@ -0,0 +1,14 @@
+//
+//  video.h
+//  maptiler2
+//
+//  Created by James Mardell on 04/12/2011.
+//  Copyright 2011 Student. All rights reserved.
+//
+
+#ifndef maptiler2_video_h
+#define maptiler2_video_h
+
+
+
+#endif
